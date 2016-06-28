a3fe189afdc4d5cb3e2d9b8171ed0623274f47f8
Rudy Gamberini <Rudy Gamberini>
2016-06-28 00:56:48 -0400

    Initial Commit

---
+++ Assets/Board.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+    public Hex centerHex;
+    private Vector3 size;
+    // Use this for initialization
+    void Start() {
+        size = centerHex.GetComponent<Collider>().bounds.size;
+        int boardSize = 2;
+
+        for (int yCoord = -boardSize; yCoord <= boardSize; yCoord++) {
+            for (int xCoord = -boardSize; xCoord <= boardSize; xCoord++) {
+                int distance = axialHexdistance(new int[] { xCoord, yCoord }, new int[] { 0, 0 });
+                if (distance <= boardSize) createHex(xCoord, yCoord);
+            }
+        }
+    }
+
+    private void createHex(int xCoord, int yCoord) {
+        // Create a new hex
+        Hex newHex = Instantiate(centerHex);
+        newHex.xCoord = xCoord;
+        newHex.yCoord = yCoord;
+
+        Vector3 localPosition = gridCoordstoWorldCoords(xCoord, yCoord);
+
+        // The transform is then applyled
+        newHex.transform.localPosition = localPosition;
+        // This stops lines from appearing between hexes
+        newHex.transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+
+        newHex.transform.parent = this.transform;
+    }
+
+    public Vector3 gridCoordstoWorldCoords(int xCoord, int yCoord) {
+        Vector3 localPosition = new Vector3();
+        // First X and Y are calculated from the Y coordinate
+        // The Y coordinate represents which axial the hex falls on
+        localPosition.x = yCoord * (size.x / -2f);
+        localPosition.z = yCoord * (size.z - (size.z / 4));
+
+        // After that the X coordinate is used to place the hex within it's row
+        localPosition.x = localPosition.x + (xCoord * size.x);
+        return localPosition;
+    }
+
+    private int axialHexdistance(int[] xy1, int[] xy2) {
+        int deltaX = xy1[0] - xy2[0];
+        int deltaY = xy1[1] - xy2[1];
+        int deltaZ = deltaX - deltaY;
+        return Mathf.Max(Mathf.Abs(deltaX), Mathf.Abs(deltaY), Mathf.Abs(deltaZ));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/ShowHover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHover : MonoBehaviour {
+    public GameObject roadPrefab;
+    private GameObject roadInstance;
+
+	// Use this for initialization
+	void Start () {
+        roadInstance = Instantiate(roadPrefab);
+        roadInstance.transform.parent = this.transform;
+        roadInstance.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    IEnumerator updateRoadInstance(Vector3 position, float angle) {
+        yield return new WaitForSeconds(2);
+        roadInstance.transform.localPosition = position;
+        Debug.Log(position);
+        roadInstance.transform.eulerAngles = new Vector3(0, angle);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100.0f)) {
+            if(hit.collider.name.Contains("Hexagon")) {
+                Hex hex = hit.collider.gameObject.GetComponent<Hex>();
+                Vector3 localHit = hit.point;
+
+                localHit = hex.transform.InverseTransformPoint(localHit);
+
+                float angleFromCenter = Mathf.Atan2(localHit.z, localHit.x) * 180 / Mathf.PI;
+                Hex.Direction direction = hex.angleToDirection((int) Mathf.Round(angleFromCenter));
+
+                Vector3 transformPosition = hex.transform.TransformPoint(
+                    hex.directionToLocalPosition(direction, roadInstance));
+
+                float angleToRotate = hex.directionToRotateAngle(direction);
+
+                StartCoroutine(updateRoadInstance(transformPosition, angleToRotate));
+            }
+        }
+    }
+}
